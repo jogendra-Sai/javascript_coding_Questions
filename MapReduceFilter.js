704c5651222a9d43479bc15c886b17ca6f0335cf
Jogendra Sai Babu Nandyala <106899455+jogendra-Sai@users.noreply.github.com>
2024-11-19 16:05:25 +0530

    Update MapReduceFilter.js

--- MapReduceFilter.js
+++ MapReduceFilter.js
@@ -10,3 +10,30 @@ const result = students.map((student)=>{
     return {name:student.name,scores:student.scores,average:sum/student.scores.length}
 }).filter(topper=>topper.average > 90)
 console.log(result)
+
+
+const products = [
+  { name: "Product 1", price: 20, category: "Electronics" },
+  { name: "Product 2", price: 30, category: "Clothes" },
+  { name: "Product 3", price: 40, category: "Electronics" },
+  { name: "Product 4", price: 50, category: "Clothes" },
+  { name: "Product 5", price: 60, category: "Clothes" },
+  { name: "Product 6", price: 70, category: "Electronics" },
+  { name: "Product 7", price: 80, category: "Clothes" },
+  { name: "Product 8", price: 90, category: "Electronics" },
+];
+
+const productsByCategory = products.reduce((acc,product)=>{
+    const category = product.category;
+    if(!acc[category]){
+       acc[category] = []; 
+    }
+    acc[category].push(product);
+    return acc;
+}, {});
+console.log(productsByCategory)
+const avgPriceByCategory = Object.keys(productsByCategory).map(category=>{
+    const sum = productsByCategory[category].reduce((acc,curr)=>acc+curr.price,0);
+    return {category:category,average:sum/productsByCategory[category].length}
+})
+console.log(avgPriceByCategory)
