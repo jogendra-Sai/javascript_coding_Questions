d1f2991dc9c6cb80a4d5c3cfb2ab7bccb756fc9e
Jogendra Sai Babu Nandyala <106899455+jogendra-Sai@users.noreply.github.com>
2025-02-13 10:02:05 +0530

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -31,3 +31,41 @@ Promise.all([func1(),func2(),func3()]).then(dataArrayValues=>{
 }).catch(error=>{
     console.log("Error",error)
 })
+
+//Promise AllSettled Example
+
+//Promise All example
+const func1 = () =>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const data = [1,2,3,4,5]
+            resolve(data)
+        },3000)
+    })
+};
+
+const func2 = ()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const data1 = [6,7,8,9,10]
+            reject("Function 2 rejected")
+        },3000)
+    })
+}
+
+const func3 = ()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const data3 = [11,12,13,14,15]
+            resolve(data3)
+        },3000)
+    })
+}
+
+Promise.allSettled([func1(),func2(),func3()]).then(dataArrayValues=>dataArrayValues.forEach(result=>{
+    if(result.status === "fulfilled"){
+        console.log("Value", result.value)
+    }else{
+        console.error("Error for rejection",result.reason)
+    }
+}))
