5e4447efdb6a7f2b20f5ea78b4a8ebe0a74f9296
Jogendra Sai Babu Nandyala <106899455+jogendra-Sai@users.noreply.github.com>
2025-03-13 22:23:39 +0530

    Update PromiseMethods.js

--- promises/PromiseMethods.js
+++ promises/PromiseMethods.js
@@ -17,8 +17,9 @@ const promise3 = new Promise((resolve,reject)=>{
 })
 
 Promise.all[promise1,promise2,promise3].then((res)=>{console.log(res)})
-.catch((error)=>{console.log(error)})
+.catch((error)=>{console.log(error)})    // Waits for all promises to resolve and returns their results as an array. If any promise is rejected, it immediately rejects.
 
 //In Success case it will return array of 3 promises results after 2 seconds(max time by any promise)
 
 //In failure case the failed promise will return after its delay
+
